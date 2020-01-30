26dc864defadfce584acc88ee97cbf9c9e762dec
jusdorge <jusdorge@gmail.com>
2020-01-30 17:28:47 +0100

    first commit

---
+++ app/src/main/java/com/example/myapplication/NumbersActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        //add an array of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        for (int i=0; i < words.size(); i++){
+            TextView wordView = new TextView(NumbersActivity.this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+    }
+}
